f86d941435261e4c2211276f3b12edace4e824f1
ArnobMahmud <arnob.tech.me@gmail.com>
2020-07-10 15:25:55 +0600

    Log-in UI

---
+++ log_in_UI/lib/Home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'Interface/signinOne.dart';
+
+// ignore: unused_import
+import 'Interface/signinTwo.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SignInOne(),
+
+//    body: SignInTwo(),
+    );
+  }
+}
