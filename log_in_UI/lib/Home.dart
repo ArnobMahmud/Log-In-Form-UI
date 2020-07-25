ed3894219ac95f8d480fd171b1fb707e14eac091
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-07-25 08:50:56 +0600

    Update Home.dart

--- log_in_UI/lib/Home.dart
+++ log_in_UI/lib/Home.dart
@@ -7,7 +7,7 @@ import 'Interface/signinTwo.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( 
       body: SignInOne(),
 
 //    body: SignInTwo(),
